717ffd6ca8a60fc123780d6e2283038eee70e530
Mariia Sirenko <79446249+mariiasirenko2@users.noreply.github.com>
2022-11-18 00:29:29 +0200

    Create ProgramCode.java

---
+++ ProgramCode.java
@@ -0,0 +1,23 @@
+package com.nuop.sdt;
+
+public class Reminder {
+
+  private Long id,
+  private String name;
+  private String days,
+  private Date time;
+  
+  
+  public void setDayToReminder(String days){
+    if(new String("ПН ВТ СР ЧТ ПТ СБ ВС").contains(days){
+        this.day = days;
+    }
+    else throws IllegalArgumentException("Allowed days: ПН ВТ СР ЧТ ПТ СБ ВС ");
+  
+  }
+
+  public static void main(String[] args) {
+    Reminder reminder = new Reminder();
+    reminder.setDayToReminder("СР");
+  }
+}
