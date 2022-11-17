cc718cc36e3e904fb81aefa17d351f7fd74395eb
Mariia Sirenko <79446249+mariiasirenko2@users.noreply.github.com>
2022-11-18 00:57:44 +0200

    Update ProgramCode.java

--- ProgramCode.java
+++ ProgramCode.java
@@ -1,19 +1,18 @@
-package com.nuop.sdt;
 import java.util.Date;
 
 public class Reminder {
 
-  public Long id,
+  public Long id;
   public String name;
-  public String days,
+  public String days;
   public Date time;
-  
+
   public void setDayToReminder(String days){
-    if(new String("ПН ВТ СР ЧТ ПТ СБ ВС").contains(days){
-        this.day = days;
+    if(new String("ПН ВТ СР ЧТ ПТ СБ ВС").contains(days)) {
+      this.days = days;
     }
     else System.out.println("Allowed days: ПН ВТ СР ЧТ ПТ СБ ВС ");
-  
+
   }
 
   public static void main(String[] args) {
