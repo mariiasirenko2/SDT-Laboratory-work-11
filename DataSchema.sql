81409632d7bf2cf54a502ada1e0a21f6bec19721
Mariia Sirenko <79446249+mariiasirenko2@users.noreply.github.com>
2022-11-17 23:41:54 +0200

    Update DataSchema.sql

--- DataSchema.sql
+++ DataSchema.sql
@@ -1,66 +1,44 @@
-
-drop table users CASCADE ;
-drop table shedule CASCADE ;
-drop table reminder CASCADE ;
-drop table device CASCADE ;
-drop table collections CASCADE ;
-drop table project CASCADE ;
-drop table editor_user CASCADE ;
-drop table graphic_editor CASCADE ;
-
-CREATE TABLE users( --опис користувача
-    user_id INT PRIMARY KEY, --айді
-	name VARCHAR, --ім'я
-	surname VARCHAR, --прізвище
-	email VARCHAR CHECK(email ~* '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$') UNIQUE --пошта
+create table users( 
+    user_id INT PRIMARY KEY, 
+	name VARCHAR, 
+	surname VARCHAR, 
+	email VARCHAR CHECK(email ~* '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$') UNIQUE 
 );
-
-CREATE TABLE reminder( --назадування
-     reminder_id INT PRIMARY KEY, --айді нагадування
-     name VARCHAR, --назва
-	 days VARCHAR, --дні коли робити нагадування
-     time timestamp --час нагадування
-   
+create table reminder(
+     reminder_id INT PRIMARY KEY,
+     name VARCHAR,
+     days VARCHAR,
+     time timestamp
 );
-
-CREATE TABLE device( --гаджет
-     device_id INT PRIMARY KEY, --айді гаджету
-     name VARCHAR, -- назва
+create table device(
+     device_id INT PRIMARY KEY,
+     name VARCHAR
 );
-
-
-CREATE TABLE shedule(  --розклад
-   user_id INT REFERENCES users(user_id), --айді користувача
-   reminder_id INT REFERENCES reminder(reminder_id), --айді наадування 
-   device_id INT REFERENCES device(device_id) --айді девайсу
+create table shedule( 
+   user_id INT REFERENCES users(user_id), 
+   reminder_id INT REFERENCES reminder(reminder_id),
+   device_id INT REFERENCES device(device_id)
 );
-
-CREATE TABLE project( --проект
-     project_id INT PRIMARY KEY, --айді проекту
-     name VARCHAR, --назва проекту
-	 details VARCHAR, --опис проекту
-     keywords VARCHAR, --ключові слова проекту
-     --ключовий колів типу #11224h
+create table project( 
+     project_id INT PRIMARY KEY,
+     name VARCHAR, 
+	 details VARCHAR,
+     keywords VARCHAR, 
      keycolor VARCHAR CHECK( keycolor ~* '#([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3}$')
-   
 );
-
-CREATE TABLE collections( --колекція
-     collections_id INT PRIMARY KEY, --айді колекцій
-     name VARCHAR, --назва колекції
-	 reference bytea, --референ картинка
-     user_id INT REFERENCES users(user_id), --айді власника
-     project_id INT REFERENCES project(project_id)    --айді проекту
+create table collections( 
+     collections_id INT PRIMARY KEY, 
+     name VARCHAR, 
+	 reference bytea, 
+     user_id INT REFERENCES users(user_id),
+     project_id INT REFERENCES project(project_id) 
 );
-
-CREATE TABLE graphic_editor( --графічний редактор
-     graphic_editor_id INT PRIMARY KEY, --айді редактору
-     name VARCHAR, --назва 
-     type VARCHAR --тип
-   
+create table graphic_editor(
+     graphic_editor_id INT PRIMARY KEY, 
+     name VARCHAR, 
+     type VARCHAR
 );
-
-CREATE TABLE editor_user( --підв'язані графічні реждактори
-     user_id INT REFERENCES users(user_id), --айді користувача
-     graphic_editor_id INT REFERENCES graphic_editor(graphic_editor_id) --айді редактору
+create table editor_user(
+     user_id INT REFERENCES users(user_id),
+     graphic_editor_id INT REFERENCES graphic_editor(graphic_editor_id)
 );
