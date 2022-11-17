034cd0c25ab92124da1dd4a8b4ff0c87984e530e
Mariia Sirenko <79446249+mariiasirenko2@users.noreply.github.com>
2022-11-17 23:47:12 +0200

    Update DataSchema.sql

--- DataSchema.sql
+++ DataSchema.sql
@@ -1,44 +1,8 @@
-create table users( 
-    user_id INT PRIMARY KEY, 
-	name VARCHAR, 
-	surname VARCHAR, 
-	email VARCHAR CHECK(email ~* '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$') UNIQUE 
-);
-create table reminder(
-     reminder_id INT PRIMARY KEY,
-     name VARCHAR,
-     days VARCHAR,
-     time timestamp
-);
-create table device(
-     device_id INT PRIMARY KEY,
-     name VARCHAR
-);
-create table shedule( 
-   user_id INT REFERENCES users(user_id), 
-   reminder_id INT REFERENCES reminder(reminder_id),
-   device_id INT REFERENCES device(device_id)
-);
-create table project( 
-     project_id INT PRIMARY KEY,
-     name VARCHAR, 
-	 details VARCHAR,
-     keywords VARCHAR, 
-     keycolor VARCHAR CHECK( keycolor ~* '#([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3}$')
-);
-create table collections( 
-     collections_id INT PRIMARY KEY, 
-     name VARCHAR, 
-	 reference bytea, 
-     user_id INT REFERENCES users(user_id),
-     project_id INT REFERENCES project(project_id) 
-);
-create table graphic_editor(
-     graphic_editor_id INT PRIMARY KEY, 
-     name VARCHAR, 
-     type VARCHAR
-);
-create table editor_user(
-     user_id INT REFERENCES users(user_id),
-     graphic_editor_id INT REFERENCES graphic_editor(graphic_editor_id)
-);
+create table users(user_id int primary key, name varchar, surname varchar, email varchar check(email ~* '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$') unique);
+create table reminder(reminder_id int primary key, name varchar, days varchar, time timestamp);
+create table device( device_id int primary key, name varchar);
+create table shedule( user_id int references users(user_id), reminder_id int references reminder(reminder_id), device_id int references device(device_id));
+create table project( project_id int primary key, name varchar, details varchar, keywords varchar, keycolor varchar check( keycolor ~* '#([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3}$'));
+create table collections( collections_id int primary key, name varchar, reference bytea, user_id int references users(user_id), project_id int references project(project_id));
+create table graphic_editor(graphic_editor_id int primary key, name varchar, type varchar);
+create table editor_user(user_id INT references users(user_id), graphic_editor_id int references graphic_editor(graphic_editor_id));
