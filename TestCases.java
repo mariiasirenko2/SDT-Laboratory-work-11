b73b61b9774e0d76eaf35e503ac9470a16ae5004
Mariia Sirenko <79446249+mariiasirenko2@users.noreply.github.com>
2022-11-20 00:22:22 +0200

    Update TestCases.java

--- TestCases.java
+++ TestCases.java
@@ -13,13 +13,12 @@ class TestCases {
             args[1]);
 
          Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery(
-          "create or replace function check_reminder_name(reminder_name varchar)\n"
-              + "return boolean\n"
-              + "is\n"
-              + "begin\n"
-              + "return not regexp_like(reminder_name, '^([^a-zA-Z]\\D*)|(\\S{18,})$');\n"
-              + "end");
+       ResultSet rs3 =stmt.executeQuery("create or replace function check_reminder_day(reminder_day varchar)\n"
+            + "return boolean\n"
+            + "is\n"
+            + "begin\n"
+            + "return  regexp_like(reminder_day, '^(\\s*ПН|ВТ|СР)');\n"
+            + "end");
         con.close();
         }catch(Exception e){
           System.out.println(e);
