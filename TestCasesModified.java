0d0ee84ab5079438a12026b9eff7a68526c0d4cb
Mariia Sirenko <79446249+mariiasirenko2@users.noreply.github.com>
2022-11-20 01:00:25 +0200

    Update TestCasesModified.java

--- TestCasesModified.java
+++ TestCasesModified.java
@@ -12,51 +12,7 @@ class TestCasesModified {
             args[0],
             args[1]);
 
-        CallableStatement cstmt1 = con.prepareCall("{? = call check_reminder(?,?)}");
-        cstmt1.registerOutParameter(1,Types.NUMERIC);
-        cstmt1.setString(2,"Breakfast");
-        cstmt1.setString(3,"ПН");
-        cstmt1.executeUpdate();
-        System.out.print("TC1: check_reminder('Breakfast','ПН') expect 1 - result:" + cstmt1.getInt(1));
-        if(cstmt1.getInt(1) == 1) 
-            System.out.println("Passed");
-        else{
-            System.out.println("Failed");
-            testcaseResult = -1;
-        }
-
-
-        cstmt1.setString(2,"akdcsdkvfdjsvnjkfkfbjfv");
-        cstmt1.setString(3,"ПН");
-        cstmt1.executeUpdate();
-        System.out.print("TC2: check_reminder('akdcsdkvfdjsvnjkfkfbjfv','ПН') expect -1 - result:" + cstmt1.getInt(1));
-        if(cstmt1.getInt(1) == -1) 
-            System.out.println("Passed");
-        else{
-            System.out.println("Failed");
-            testcaseResult = -1;
-        }
-
-        cstmt1.setString(2,"12 Breakfast");
-        cstmt1.setString(3,"ПН");
-        cstmt1.executeUpdate();
-        System.out.print("TC3: check_reminder('12 Breakfast','ПН') expect -1 - result:" + cstmt1.getInt(1));
-        if(cstmt1.getInt(1) == -1) 
-            System.out.println("Passed");
-        else{
-            System.out.println("Failed");
-            testcaseResult = -1;}
-
-        cstmt1.setString(2,"Breakfast");
-        cstmt1.setString(3,"monday");
-        cstmt1.executeUpdate();
-        System.out.print("TC4: check_reminder('Breakfast','monday') expect -2 - result:" + cstmt1.getInt(1));
-        if(cstmt1.getInt(1) == -1) 
-            System.out.println("Passed");
-        else{
-            System.out.println("Failed");
-            testcaseResult = -1;
-        }
+        
 
         con.close();
       }
