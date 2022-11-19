6ecf78b31c7306c18a920b35b1ac4e663ff4913b
Mariia Sirenko <79446249+mariiasirenko2@users.noreply.github.com>
2022-11-19 22:47:47 +0200

    Update ProgramCodeModified.sql

--- ProgramCodeModified.sql
+++ ProgramCodeModified.sql
@@ -9,31 +9,5 @@ CREATE OR REPLACE TABLE Users(
 
 /* Створення типу для зберігання списку користувачів - "Колекція екземплярів об`єктів класу Users */
 CREATE TABLE Users_List IS TABLE OF users;
-/
 
-
-/* Створення об'єктного типу (класу) Project */
-CREATE OR REPLACE TABLE Project (
-	project_id NUMBER(10), --айді проекту
-	name VARCHAR(100),--назва проекту
-	details VARCHAR(100), --опис проекту
-        keywords VARCHAR(100)--ключові слова проект
-      )
-	
-
-    	CONSTRUCTOR FUNCTION Project(p_pname VARCHAR)
-        	RETURN SELF AS RESULT,
-		/* Процедура зміни значення атрибутів */
-		MEMBER PROCEDURE set_pname(p_product_id NUMBER, p_pname VARCHAR, details VARCHAR),
-    		/* Функції отримання значень атрибутів */
-		MEMBER FUNCTION get_project_id RETURN NUMBER,
-		MEMBER FUNCTION get_pname RETURN VARCHAR,
-		MEMBER FUNCTION get_details RETURN VARCHAR,
-        MEMBER FUNCTION get_keywords RETURN VARCHAR,
-		MEMBER FUNCTION get_keycolor RETURN VARCHAR,
-        MEMBER FUNCTION get_collections RETURN Collection_List,
-
-	/* Процедура виводу на екран значень атрибутів */
-    	MEMBER PROCEDURE display
-); 
 
